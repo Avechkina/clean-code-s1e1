a06b78e132d99a48170b08f4773f2f90e47b4081
Наташка Овечкина <natallia.ovechkina@gmail.com>
2024-12-12 16:23:20 +0300

    refactor(extended-1.2): add alternative text for images

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='delete button';
     deleteButtonImg.classList.add("img-delete");
     deleteButton.appendChild(deleteButtonImg);
 
